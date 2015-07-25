34ab9f218b6cc90c984b6b7cec2e57052a79b4e6
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-07-25 16:26:06 +0430

    Added a dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app/main.js'),
+  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
