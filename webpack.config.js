45ac3a73483305edfdc5f33451fcdbd85c32adfb
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-07-25 17:11:54 +0430

    Optimized react bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 
 var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
 var config = {
   entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+  resolve: {
+    alias: {
+      'react': pathToReact
+    }
+  },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
@@ -13,7 +21,8 @@ var config = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel'
       }
-    ]
+    ],
+    noParse: [pathToReact]
   }
 };
 
