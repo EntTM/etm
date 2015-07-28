59f71d0652ef7997d9678c07d8e476f608746f96
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-07-28 21:57:58 +0430

    Added flow type checking to tests.

--- test/component-test.js
+++ test/component-test.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 import test from 'tape';
 import createComponent from './create-component';
 import Hello from '../app/component';
@@ -5,7 +7,7 @@ import Hello from '../app/component';
 
 
 test("Hello component", (t) => {
-  const result = createComponent(Hello);
+  var result = createComponent(Hello, {});
 
   t.equal(result.type, 'h1', 'Should be an h1.');
   t.equal(result.props.children, 'Hello World!', 'Should have hello world.')
--- test/create-component.js
+++ test/create-component.js
@@ -1,10 +1,15 @@
+/* @flow */
+
 import React from 'react/addons';
-const TestUtils = React.addons.TestUtils;
+var TestUtils = React.addons.TestUtils;
 
 export default createComponent;
 
-function createComponent(component, props, ...children) {
-  const shallowRenderer = TestUtils.createRenderer();
+function createComponent(
+  component: React.Element,
+  props: Object,
+  ...children: Array<React.Element>): React.Component {
+  var shallowRenderer = TestUtils.createRenderer();
   shallowRenderer.render(React.createElement(component, props, children.length > 1 ? children : children[0]));
   return shallowRenderer.getRenderOutput();
 }
