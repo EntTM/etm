e73cff5ec156d44077af2fce74fbaf3ffcee6179
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 21:48:09 +0430

    Imported AppSettings page

--- app/routes.js
+++ app/routes.js
@@ -9,17 +9,17 @@ import AllActivities from './components/all-activities-page';
 // import CurrentActivities from './components/pages/current-activities';
 // import Dashboard from './components/pages/dashboard';
 // import ProfileSettings from './components/pages/profile-settings';
-// import AppSettings from './components/pages/app-settings';
+import AppSettings from './components/app-settings-page';
 
 var routes = (
   <Route handler={Master} name="root" path="/">
     <Route handler={Login} name="login" />
     <Route handler={AllActivities} name="all-activities" />
+    <Route handler={AppSettings} name="app-settings" />
     {/*
     <Route handler={CurrentActivities} name="current-activities" />
     <Route handler={Dashboard} name="dashboard" />
     <Route handler={ProfileSettings} name="profile-settings" />
-    <Route handler={AppSettings} name="app-settings" />
     */}
 
     <DefaultRoute handler={Login}/>
