c6828cbcf510e63a395ff5f0f40bf75d054979a4
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 23:08:06 +0430

    Imported DashboardPage

---
+++ app/components/dashboard-page.js
@@ -0,0 +1,44 @@
+/* @flow */
+
+import React from 'react';
+import {CircularProgress, Styles} from 'material-ui';
+
+var ThemeManager = new Styles.ThemeManager();
+var Colors = Styles.Colors;
+
+class Dashboard extends React.Component {
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
+  componentWillMount() {
+    ThemeManager.setPalette({
+      accent1Color: Colors.deepOrange500
+    });
+  }
+
+  render() {
+    var containerStyle = {
+      textAlign: 'center',
+      paddingTop: '50px'
+    };
+
+    return (
+      <div style={containerStyle}>
+        <h1>Enterprise Task Manager</h1>
+        <h2>Dashboard</h2>
+        <CircularProgress mode="indeterminate" />
+      </div>
+    );
+  }
+
+}
+
+Dashboard.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
+export default Dashboard;
