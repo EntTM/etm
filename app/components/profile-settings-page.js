505af005b3491102430330778d82c314c4077e25
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 23:36:55 +0430

    Imported ProfileSettings

---
+++ app/components/profile-settings-page.js
@@ -0,0 +1,52 @@
+/* @flow */
+
+import React from 'react';
+import {
+  MenuItem,
+  List,
+  ListDivider,
+  ListItem,
+  Styles
+} from 'material-ui';
+
+var Colors = Styles.Colors;
+
+class ProfileSettings extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <List subheader={<div style={{paddingRight:'16px'}}>تنظیمات عمومی</div>}>
+          <ListItem
+            primaryText="نام و نام خانوادگی"
+            secondaryText="حسین نیکپور" />
+
+          <ListItem
+            primaryText="نام کاربری"
+            secondaryText="HosseinNikpour" />
+
+          <ListItem
+            primaryText="رمز عبور" />
+
+          <ListItem
+            primaryText="تصویر پروفایل" />
+        </List>
+
+        <ListDivider />
+
+        <List subheader={<div style={{paddingRight:'16px'}}>تنظیمات دیگر</div>}>
+          <ListItem
+            primaryText="لغو اشتراک برنامه"
+            secondaryText="تمام داده‌ها پاک خواهد شد" />
+        </List>
+      </div>
+    );
+  }
+
+}
+
+export default ProfileSettings;
