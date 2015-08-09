1951601eec8e988960303a30fafdb4b77c2f53c8
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 15:04:22 +0430

    Refactored redux Provider to main.js

--- app/components/app.js
+++ app/components/app.js
@@ -1,24 +1,13 @@
 /* @flow */
 
 import React from 'react';
-import {combineReducers, createStore} from 'redux';
-import * as reducers from '../reducers';
-import {Provider} from 'react-redux';
 import {RouteHandler} from 'react-router';
 
 
 
-var app   = combineReducers(reducers),
-    store = createStore(app);
-
-
 class App extends React.Component {
   render(): React.Element {
-    return (
-      <Provider store={store}>
-      {() => <RouteHandler />}
-      </Provider>
-    );
+    return <RouteHandler />;
   }
 }
 
--- app/main.js
+++ app/main.js
@@ -5,16 +5,25 @@ import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Router from 'react-router';
 import routes from './routes';
+import {Provider} from 'react-redux';
+import {combineReducers, createStore} from 'redux';
+import * as reducers from './reducers';
 
 
 
 injectTapEventPlugin();
 
 
+var app   = combineReducers(reducers),
+    store = createStore(app);
+
+
 function main() {
   Router.run(routes, Router.HashLocation, (Root) => {
     React.render(
-      <Root />,
+      <Provider store={store}>
+        {() => <Root />}
+      </Provider>,
       document.getElementById('app'));
   });
 }
