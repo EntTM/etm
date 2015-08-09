b8256527c58f130d5e5096d337b63361dc4a4b20
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 21:39:14 +0430

    Imported AllActivities from Hossein's works

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ import {Route, DefaultRoute, NotFoundRoute} from 'react-router';
 import NotFound from './components/not-found';
 import Master from './components/master';
 import Login from './components/login-page';
-// import AllActivities from './components/pages/all-activities';
+import AllActivities from './components/all-activities-page';
 // import CurrentActivities from './components/pages/current-activities';
 // import Dashboard from './components/pages/dashboard';
 // import ProfileSettings from './components/pages/profile-settings';
@@ -14,8 +14,8 @@ import Login from './components/login-page';
 var routes = (
   <Route handler={Master} name="root" path="/">
     <Route handler={Login} name="login" />
-    {/*
     <Route handler={AllActivities} name="all-activities" />
+    {/*
     <Route handler={CurrentActivities} name="current-activities" />
     <Route handler={Dashboard} name="dashboard" />
     <Route handler={ProfileSettings} name="profile-settings" />
