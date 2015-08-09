d4ca9df998bd9cda636ed518a3ab464fccebdc28
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-09 13:20:18 +0430

    Added a not found route

---
+++ app/components/not-found.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+import React from 'react';
+
+
+
+class NotFound extends React.Component {
+  render(): React.Element {
+    return <div>404 Not Found!</div>;
+  }
+}
+
+export default NotFound;
--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,18 @@
 /* @flow */
 
 import React from 'react';
-import {Route, DefaultRoute} from 'react-router';
+import {Route, DefaultRoute, NotFoundRoute} from 'react-router';
 import App from './components/app';
 import Hello from './components/hello';
+import NotFound from './components/not-found';
 
 
 
 var routes = (
   <Route handler={App}>
     <DefaultRoute handler={Hello} />
+
+    <NotFoundRoute handler={NotFound} />
   </Route>
 );
 
