03f1c3cd923c6db7891eaf1f0b9a42a4ad972655
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-10 11:25:24 +0430

    Added XDK

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var node_modules = path.resolve(__dirname, 'node_modules');
 // var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 var config = {
-  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+  entry: [/* 'webpack/hot/dev-server',  */path.resolve(__dirname, 'app/main.js')],
   // resolve: {
   //   alias: {
   //     'react': pathToReact
