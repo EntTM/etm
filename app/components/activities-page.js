d92b16fd4edd04fd03d209b9c032da4f12cede8f
HosseinMoradiDavijani <hossein.moradi@yahoo.com>
2015-08-15 05:07:54 +0430

    UI Improvements

--- app/components/activities-page.js
+++ app/components/activities-page.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 import React from 'react';
-import rd3 from 'react-d3-components';
 import {
   Avatar,
   Card,
@@ -17,11 +16,18 @@ import {
   Styles
 } from 'material-ui';
 
-var Transitions = Styles.Transitions;
 var ThemeManager = new Styles.ThemeManager();
 var Colors = Styles.Colors;
 
 class Activities extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {isExpanded: false}
+    this._handleFabTouch = this._handleFabTouch.bind(this)
+    this._handleJobDone = this._handleJobDone.bind(this)
+    this._handleSnackbarAction = this._handleSnackbarAction.bind(this)
+  }
 
   getChildContext() {
     return {
@@ -43,11 +49,20 @@ class Activities extends React.Component {
       fabStyle: {
         position: 'fixed',
         right: '15px',
-        bottom: '25px',
+        bottom: '24px',
         zIndex: '999',
         direction: 'ltr',
-        transition: 'transform 0.5s',
+        transition: 'transform 0.2s',
         transitionTimingFunction: 'ease-in'
+      },
+      filterFabsStyle: {
+        position: 'fixed',
+        right: '22px',
+        bottom: '32px',
+        zIndex: '990',
+        direction: 'ltr',
+        transition: 'bottom 0.2s',
+        transitionTimingFunction: 'ease-in',
       }
     };
   }
@@ -61,62 +76,69 @@ class Activities extends React.Component {
   }
 
   _handleFabTouch() {
-    this.refs.snackbar.show();
+    this.setState({isExpanded: !this.state.isExpanded});
+  }
+
+  _filterPastTasks() {
+    console.log('Filter tasks and show just past Tasks');
+  }
+
+  _filterCurrentTasks() {
+    console.log('Filter tasks and show just current Tasks');
+  }
+
+  _filterFutureTasks() {
+    console.log('Filter tasks and show just future Tasks');
   }
 
   render() : React.Element {
     var styles = this.getStyles();
 
     var avatar = (
-      <Avatar backgroundColor={Colors.greenA700} />
+      <FontIcon className="material-icons" style={{direction: 'ltr', fontSize: '40px'}}>assignment</FontIcon>
     );
-
-    var avatar2 = (
-      <Avatar backgroundColor={Colors.redA700} />
-    );
-
-    var avatar3 = (
-      <Avatar backgroundColor={Colors.yellowA700} />
-    );
-
-    var avatar4 = (
-      <Avatar backgroundColor={Colors.greenA700} />
-    );
-
-    // var fabIcon = (this.isExpanded)?
-    //   <FontIcon className="material-icons">clear</FontIcon> :
-    //     <FontIcon className="material-icons">add</FontIcon>;
-
-    var BarChart = rd3.BarChart;
 
     var data = [{
       label: 'somethingA',
       values: [{x: 'SomethingA', y: 10}, {x: 'SomethingB', y: 4}, {x: 'SomethingC', y: 3}]
     }];
 
+    var rotate45 = {transform: 'rotate(45deg)'};
     return (
       <div style={styles.containerStyle}>
         <FloatingActionButton
-          isExpanded={false}
           onTouchTap={this._handleFabTouch}
-          ref="fabButton"
-          style={styles.fabStyle}>
+          ref="fab"
+          style={Object.assign({}, styles.fabStyle, this.state.isExpanded && rotate45)} >
           <FontIcon className="material-icons" ref="fabIcon">add</FontIcon>
         </FloatingActionButton>
-
+        <div>
+          <FloatingActionButton
+            mini={true}
+            onTouchTap={this._filterCurrentTasks}
+            style={Object.assign({}, styles.filterFabsStyle, this.state.isExpanded && {bottom: '100px'})} >
+            <FontIcon className="material-icons" ref="fabIcon">play_arrow</FontIcon>
+          </FloatingActionButton>
+          <FloatingActionButton
+            mini={true}
+            onTouchTap={this._filterPastTasks}
+            style={Object.assign({}, styles.filterFabsStyle, this.state.isExpanded && {bottom: '150px'})} >
+            <FontIcon className="material-icons" ref="fabIcon">done</FontIcon>
+          </FloatingActionButton>
+          <FloatingActionButton
+            mini={true}
+            onTouchTap={this._filterFutureTasks}
+            style={Object.assign({}, styles.filterFabsStyle, this.state.isExpanded && {bottom: '200px'})} >
+            <FontIcon className="material-icons" ref="fabIcon">trending_up</FontIcon>
+          </FloatingActionButton>
+        </div>
         <Snackbar
           action="لغو"
           autoHideDuration={0}
           message="فعالیت مورد نظر تمام شد"
           onActionTouchTap={this._handleSnackbarAction}
           ref="snackbar"
-          style={{direction: 'rtl', float:'left', textAlign:'left'}}/>
-
-          <BarChart
-              data={data}
-              height={400}
-              margin={{top: 10, bottom: 50, left: 50, right: 10}}
-              width={window.innerWidth}/>
+          style={{minWidth: '100px', paddingRight: '0', paddingLeft: '12px'}}/>
 
         <Card initiallyExpanded={true}>
           <CardHeader
@@ -131,13 +153,18 @@ class Activities extends React.Component {
             <h3>توضیحات:</h3>
             <p>بهبود عملکرد ماژول ایگرگ در صفحه‌ی زد</p>
           </CardText>
+          <CardActions expandable={true}>
+            <RaisedButton
+              label="اتمام کار"
+              onTouchTap={this._handleJobDone}/>
+          </CardActions>
         </Card>
 
         <div style={{height:'5px'}}></div>
 
         <Card initiallyExpanded={true}>
           <CardHeader
-            avatar={avatar2}
+            avatar={avatar}
             showExpandableButton={true}
             style={{paddingRight: '40px'}}
             subtitle="پروژه‌ی ایکس"
@@ -148,13 +175,18 @@ class Activities extends React.Component {
             <h3>توضیحات:</h3>
             <p>رفع مشکل شماره 541 در صفحه‌ی دوم سایت</p>
           </CardText>
+          <CardActions expandable={true}>
+            <RaisedButton
+              label="اتمام کار"
+              onTouchTap={this._handleJobDone}/>
+          </CardActions>
         </Card>
 
         <div style={{height:'5px'}}></div>
 
         <Card initiallyExpanded={true}>
           <CardHeader
-            avatar={avatar3}
+            avatar={avatar}
             showExpandableButton={true}
             style={{paddingRight: '40px'}}
             subtitle="پروژه‌ی شرکت دبلیو"
@@ -165,13 +197,18 @@ class Activities extends React.Component {
             <h3>توضیحات:</h3>
             <p>توسعه‌ی ماژول دلتا</p>
           </CardText>
+          <CardActions expandable={true}>
+            <RaisedButton
+              label="اتمام کار"
+              onTouchTap={this._handleJobDone}/>
+          </CardActions>
         </Card>
 
         <div style={{height:'5px'}}></div>
 
         <Card initiallyExpanded={true}>
           <CardHeader
-            avatar={avatar4}
+            avatar={avatar}
             showExpandableButton={true}
             style={{paddingRight: '40px'}}
             subtitle="پروژه‌ی شرکت دبلیو"
@@ -182,6 +219,11 @@ class Activities extends React.Component {
             <h3>توضیحات:</h3>
             <p>ساخت صفحات مربوط به ماژول تتا</p>
           </CardText>
+          <CardActions expandable={true}>
+            <RaisedButton
+              label="اتمام کار"
+              onTouchTap={this._handleJobDone}/>
+          </CardActions>
         </Card>
       </div>
     );
