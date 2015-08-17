2a684b76ca9b24042c0870ada62ce60d60b28b28
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-17 19:58:06 +0430

    Imported AllActivities from Hossein's works

--- app/components/all-activities-page.js
+++ app/components/all-activities-page.js
@@ -1,5 +1,7 @@
-let React = require('react');
-let {
+/* @flow */
+
+import React from 'react';
+import {
   Avatar,
   Card,
   CardActions,
@@ -10,28 +12,24 @@ let {
   RaisedButton,
   Snackbar,
   Styles
-  } = require('material-ui');
+} from 'material-ui';
 
-let ThemeManager = new Styles.ThemeManager();
-let Colors = Styles.Colors;
+var ThemeManager = new Styles.ThemeManager();
+var Colors = Styles.Colors;
 
-let AllActivitiesPage = React.createClass({
-
-  childContextTypes: {
-    muiTheme: React.PropTypes.object
-  },
+class AllActivities extends React.Component {
 
   getChildContext() {
     return {
       muiTheme: ThemeManager.getCurrentTheme()
     };
-  },
+  }
 
   componentWillMount() {
     ThemeManager.setPalette({
       accent1Color: Colors.deepOrange500
     });
-  },
+  }
 
   getStyles() {
     return {
@@ -39,64 +37,117 @@ let AllActivitiesPage = React.createClass({
         textAlign: 'right'
       }
     };
-  },
+  }
+
+  _handleJobDone() {
+    this.refs.snackbar.show();
+  }
+
+  _handleSnackbarAction() {
+    this.refs.snackbar.dismiss();
+  }
 
   render() {
+    var styles = this.getStyles();
 
-    let styles = this.getStyles();
+    var avatar = (
+      <Avatar
+        backgroundColor={Colors.green500}
+        style={{color:Colors.green100}}>
+
+        X
+      </Avatar>
+    );
+
+    var avatar2 = (
+      <Avatar
+        backgroundColor={Colors.green500}
+        style={{color:Colors.green100}}>
+
+        X
+      </Avatar>
+    );
+
+    var avatar3 = (
+      <Avatar
+        backgroundColor={Colors.blue500}
+        style={{color:Colors.blue100}}>
+
+        W
+      </Avatar>
+    );
+
+    var avatar4 = (
+      <Avatar
+        backgroundColor={Colors.blue500}
+        style={{color:Colors.blue100}}>
+
+        W
+      </Avatar>
+    );
 
     return (
       <div style={styles.containerStyle}>
         <Card initiallyExpanded={true}>
-          <CardHeader style={{paddingRight: '40px'}}
-            title="فعالیت اول"
-            subtitle="پروژه‌ی ایکس"
+          <CardHeader
+            avatar={avatar}
             showExpandableButton={true}
-            avatar={<Avatar backgroundColor={Colors.green500} style={{color:Colors.green100}}>X</Avatar>}>
-          </CardHeader>
-          <CardText style={styles.rtl} expandable={true}>
+            style={{paddingRight: '40px'}}
+            subtitle="پروژه‌ی ایکس"
+            title="فعالیت اول" />
+
+          <CardText expandable={true} style={styles.rtl}>
             <h3>انجام شده در زمان: 14 روز و 21 ساعت</h3>
             <h3>توضیحات:</h3>
             <p>بهبود عملکرد ماژول ایگرگ در صفحه‌ی زد</p>
           </CardText>
         </Card>
+
         <div style={{height:'5px'}}></div>
+
         <Card initiallyExpanded={true}>
-          <CardHeader style={{paddingRight: '40px'}}
-            title="فعالیت دوم"
-            subtitle="پروژه‌ی ایکس"
+          <CardHeader
+            avatar={avatar2}
             showExpandableButton={true}
-            avatar={<Avatar backgroundColor={Colors.green500} style={{color:Colors.green100}}>X</Avatar>}>
-          </CardHeader>
-          <CardText style={styles.rtl} expandable={true}>
+            style={{paddingRight: '40px'}}
+            subtitle="پروژه‌ی ایکس"
+            title="فعالیت دوم" />
+
+          <CardText expandable={true} style={styles.rtl}>
             <h3>انجام شده در زمان: 2 روز و 4 ساعت</h3>
             <h3>توضیحات:</h3>
             <p>رفع مشکل شماره 541 در صفحه‌ی دوم سایت</p>
           </CardText>
         </Card>
+
         <div style={{height:'5px'}}></div>
+
         <Card initiallyExpanded={true}>
-          <CardHeader style={{paddingRight: '40px'}}
-            title="فعالیت سوم"
-            subtitle="پروژه‌ی شرکت دبلیو"
+          <CardHeader
+            avatar={avatar3}
             showExpandableButton={true}
-            avatar={<Avatar backgroundColor={Colors.blue500} style={{color:Colors.blue100}}>W</Avatar>}>
-          </CardHeader>
-          <CardText style={styles.rtl} expandable={true}>
+            style={{paddingRight: '40px'}}
+            subtitle="پروژه‌ی شرکت دبلیو"
+            title="فعالیت سوم" />
+
+          <CardText expandable={true} style={styles.rtl}>
             <h3>انجام شده در زمان: 7 روز و 1 ساعت</h3>
             <h3>توضیحات:</h3>
             <p>توسعه‌ی ماژول دلتا</p>
           </CardText>
         </Card>
+
         <div style={{height:'5px'}}></div>
+
         <Card initiallyExpanded={true}>
-          <CardHeader style={{paddingRight: '40px'}}
-            title="فعالیت چهارم"
-            subtitle="پروژه‌ی شرکت دبلیو"
+          <CardHeader
+            avatar={avatar4}
             showExpandableButton={true}
-            avatar={<Avatar backgroundColor={Colors.blue500} style={{color:Colors.blue100}}>W</Avatar>}>
-          </CardHeader>
-          <CardText style={styles.rtl} expandable={true}>
+            style={{paddingRight: '40px'}}
+            subtitle="پروژه‌ی شرکت دبلیو"
+            title="فعالیت چهارم" />
+
+          <CardText expandable={true} style={styles.rtl}>
             <h3>انجام شده در زمان: 21 روز و 12 ساعت</h3>
             <h3>توضیحات:</h3>
             <p>ساخت صفحات مربوط به ماژول تتا</p>
@@ -104,16 +155,12 @@ let AllActivitiesPage = React.createClass({
         </Card>
       </div>
     );
-  },
-
-  _handleJobDone() {
-    this.refs.snackbar.show();
-  },
-
-  _handleSnackbarAction() {
-    this.refs.snackbar.dismiss();
   }
 
-});
+}
 
-module.exports = AllActivitiesPage;
+AllActivities.childContextTypes = {
+  muiTheme: React.PropTypes.object
+};
+
+export default AllActivities;
