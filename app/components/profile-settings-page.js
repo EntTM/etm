a0351ebf4b23808b08cc739578bfcd7aeb7f7c94
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-17 20:08:06 +0430

    Imported ProfileSettings

--- app/components/profile-settings-page.js
+++ app/components/profile-settings-page.js
@@ -1,41 +1,52 @@
-let React = require('react');
-let {MenuItem, List, ListDivider, ListItem, Styles} = require('material-ui');
+/* @flow */
 
-let Colors = Styles.Colors;
+import React from 'react';
+import {
+  MenuItem,
+  List,
+  ListDivider,
+  ListItem,
+  Styles
+} from 'material-ui';
 
-class ListsPage extends React.Component {
+var Colors = Styles.Colors;
+
+class ProfileSettings extends React.Component {
 
   constructor() {
     super();
   }
 
   render() {
-
     return (
       <div>
         <List subheader={<div style={{paddingRight:'16px'}}>تنظیمات عمومی</div>}>
           <ListItem
             primaryText="نام و نام خانوادگی"
             secondaryText="حسین نیکپور" />
+
           <ListItem
             primaryText="نام کاربری"
             secondaryText="HosseinNikpour" />
+
           <ListItem
             primaryText="رمز عبور" />
+
           <ListItem
             primaryText="تصویر پروفایل" />
         </List>
+
         <ListDivider />
+
         <List subheader={<div style={{paddingRight:'16px'}}>تنظیمات دیگر</div>}>
           <ListItem
             primaryText="لغو اشتراک برنامه"
             secondaryText="تمام داده‌ها پاک خواهد شد" />
         </List>
-
       </div>
     );
   }
 
 }
 
-module.exports = ListsPage;
+export default ProfileSettings;
