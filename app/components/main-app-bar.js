b5a2f226d45650a26c0b9a7582f1bb67f9aadba1
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2015-08-24 13:39:10 +0430

    First draft of main-app-bar

--- app/components/main-app-bar.js
+++ app/components/main-app-bar.js
@@ -2,34 +2,81 @@
 
 import React from 'react';
 import {connect} from 'react-redux';
-import {AppBar, CircularProgress} from 'material-ui';
+import {IconButton, FontIcon} from 'material-ui';
 import {MainAppBarSelector} from '../selectors';
 
 
 
-class MainAppBar extends React.Component {
-  render(): React.Element {
-    var loadingIcon;
-    if (this.props.loading) {
-      loadingIcon = <CircularProgress color='white' size={0.5}/>;
-    }
-    else {
-      loadingIcon = <CircularProgress mode='determinate' size={0.5} value={0}/>;
-    }
+var styles = {
+  container: {
+    direction: 'ltr',
+    height: '10vh',
+    backgroundColor: '#998E67',
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    alignItems: 'center'
+  },
+  backButton: {
+    flex: 'none'
+  },
+  projTitle: {
+    flex: 'none',
+    fontWeight: 'normal'
+  },
+  miniList: {
+    flex: '1 1 auto',
+    alignSelf: 'stretch',
+    marginLeft: '5vw',
+    backgroundColor: '#999999'
+  },
+  menu: {
+    flex: 'none'
+  }
+};
 
+class MainAppBar extends React.Component {
+  _backButton(): React.Element {
     return (
-      <div style={{height: 100}}>
-        <AppBar
-          iconElementRight={loadingIcon}
-          onLeftIconButtonTouchTap={this.props.menu}
-          showMenuIconButton={!!localStorage.getItem('loggedIn')}
-          style={{
-            direction: 'ltr',
-            position: 'fixed'
-          }}
-          title={this.props.title}
-          zDepth={0}
-        />
+      <div style={styles.backButton}>
+        <IconButton iconClassName='material-icons'>
+          arrow_back
+        </IconButton>
+      </div>
+    );
+  }
+
+  _projTitle(): React.Element {
+    return (
+      <h1 style={styles.projTitle}>
+        {this.props.title}
+      </h1>
+    );
+  }
+
+  _miniList(): React.Element {
+    return (
+      <div style={styles.miniList}>
+      </div>
+    );
+  }
+
+  _menu(): React.Element {
+    return (
+      <div style={styles.menu}>
+        <IconButton iconClassName='material-icons'>
+          more_vert
+        </IconButton>
+      </div>
+    );
+  }
+
+  render(): React.Element {
+    return (
+      <div style={styles.container}>
+        {this._backButton()}
+        {this._projTitle()}
+        {this._miniList()}
+        {this._menu()}
       </div>
     );
   }
